128c97e61c46bec5f7d3f044e7f967967cab7ce9
xiaohe <18911024431@163.com>
2019-08-13 10:10:21 +0800

    使用默认状态栏

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
 import "package:flutter/material.dart";
-import 'dart:io';
-import 'package:flutter/services.dart';
 
 import 'routes/routes.dart';
 
 void main() {
   runApp(new MyApp());
-  if (Platform.isAndroid) {
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
-  }
 }
 
 class MyApp extends StatelessWidget {
